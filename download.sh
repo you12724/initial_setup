9987ad1c4a5850066feec7a4f441e2a4f09fe966
you12724 <you12724@gmail.com>
2018-03-09 17:28:13 +0900

    brew install系を記述

--- download.sh
+++ download.sh
@@ -17,3 +17,24 @@ echo " ------------ Git ------------"
 brew install git
 git --version
 echo " ------------ END ------------"
+
+#
+# Install ruby
+#
+echo " ------------ Ruby ------------"
+brew install rbenv ruby-build
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile
+
+rbenv install $(rbenv install -l | grep -v - | tail -1)
+rbenv global $(rbenv install -l | grep -v - | tail -1)
+gem install bundler
+rbenv rehash
+echo " ------------ END ------------"
+
+#
+# Install mysql
+#
+echo " ------------ Git ------------"
+brew install mysql
+echo " ------------ END ------------"
